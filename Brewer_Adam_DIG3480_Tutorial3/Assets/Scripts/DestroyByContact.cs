ffbe25a2b8f47aec8a30122cc0021cdfb951c273
Brewer <ahcbrewer@users.noreply.github.com>
2019-11-11 00:02:31 -0500

    Files

---
+++ Brewer_Adam_DIG3480_Tutorial3/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other)
+{	
+	if (other.tag == "Boundary")
+	{
+		return;
+	}
+	Instantiate(explosion, transform.position, transform.rotation);
+	if (other.tag == "Player")
+	{
+	Instantiate(playerExplosion, transform.position, transform.rotation);
+	}
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
