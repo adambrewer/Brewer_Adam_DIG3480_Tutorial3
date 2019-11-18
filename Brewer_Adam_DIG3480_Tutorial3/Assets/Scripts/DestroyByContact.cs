1f1d023627c09396583debb77f7ebd62d5088b33
Brewer <ahcbrewer@users.noreply.github.com>
2019-11-17 22:52:34 -0500

    Update 2

--- Brewer_Adam_DIG3480_Tutorial3/Assets/Scripts/DestroyByContact.cs
+++ Brewer_Adam_DIG3480_Tutorial3/Assets/Scripts/DestroyByContact.cs
@@ -6,18 +6,40 @@ public class DestroyByContact : MonoBehaviour
 
 	public GameObject explosion;
 	public GameObject playerExplosion;
+	public int scoreValue;
+	public GameController gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <GameController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+
+
 
     void OnTriggerEnter(Collider other)
 {	
-	if (other.tag == "Boundary")
+	if (other.tag == "Boundary" || other.tag == "Enemy")
 	{
 		return;
 	}
-	Instantiate(explosion, transform.position, transform.rotation);
+	if (explosion != null)
+	{
+		Instantiate(explosion, transform.position, transform.rotation);
+	}
 	if (other.tag == "Player")
 	{
 	Instantiate(playerExplosion, transform.position, transform.rotation);
+	gameController.gameOver ();
 	}
+	gameController.AddScore (scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
--- Brewer_Adam_DIG3480_Tutorial3/Assets/Scripts/GameController.cs
+++ Brewer_Adam_DIG3480_Tutorial3/Assets/Scripts/GameController.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-	public GameObject hazard;
+	public GameObject[] hazards;
 	public Vector3 spawnValues;
 	public int hazardCount;
 	public float spawnWait;
 	public float startWait;
 	public float waveWait;
 
+	public Text ScoreText;
+	private int score;
+	
+	public Text restartText;
+	public Text gameOverText;
+	public Text winText;
+
+	private bool gameOverMan;
+	private bool restart;
+	private bool youWin;
+
+
     void Start()
     {
+	gameOverMan = false;
+	restart = false;
+	youWin = false;
+	winText.text = "";
+	restartText.text = "";
+	gameOverText.text ="";
+	score = 0;
+void UpdateScore()
+{
+ScoreText.text = "Points: " + score;
+}
 	StartCoroutine (SpawnWaves ());        
     }
 
@@ -20,6 +45,13 @@ void Update()
 {
 if (Input.GetKey("escape"))
      Application.Quit();
+if (restart)
+	{
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			SceneManager.LoadScene("SampleScene");
+		}
+	}
 }
 
     IEnumerator SpawnWaves()
@@ -30,14 +62,52 @@ if (Input.GetKey("escape"))
 {
 	for(int i = 0; i < hazardCount; i++)
 	{
+	GameObject hazard = hazards[Random.Range (0, hazards.Length)];
         Vector3 spawnPosition = new Vector3(Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 	Quaternion spawnRotation = Quaternion.identity;
 	Instantiate (hazard, spawnPosition, spawnRotation);
 	yield return new WaitForSeconds (spawnWait);
 	}
 	yield return new WaitForSeconds(waveWait);
+
+	if (gameOverMan)
+	{
+		restartText.text = "Press 'T' to Restart";
+		restart = true;
+		break;
+	}
+	if (youWin)
+	{
+		restartText.text = "Press 'T' to Restart";
+		restart = true;
+		break;
+	}
+
 }
-
-
     }
+void UpdateScore()
+{
+ScoreText.text = "Points: " + score;
 }
+
+public void AddScore(int newScoreValue)
+{
+score += newScoreValue;
+UpdateScore();
+if (score >= 100)
+	{
+	youWin = true;
+	winText.text = "You Win!  Game created by Adam Brewer.";
+	}
+}
+
+public void gameOver()
+	{
+		gameOverText.text = "Game Over!";
+		gameOverMan = true;
+	}
+
+
+}
+
+
